7f5224e470fb68fd5e27e92f6c8d2d37a2f41c5f
ButtonGame <Mandapk255@gmail.com>
2021-06-28 11:51:03 -0700

    added image slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+import ImgSlider from "./imgSlider";
 
 function Home() {
 	return (
-		<div>
-			<p></p>
-		</div>
+		<Container>
+			<ImageContainer>
+				<ImgSlider />
+			</ImageContainer>
+		</Container>
 	);
 }
 
 export default Home;
+
+const Container = styled.main`
+	min-height: 100vh;
+	position: relative;
+	&:before {
+		background: url("/images/home-background.png") center center / cover
+			no-repeat fixed;
+		content: "";
+		position: absolute;
+		top: 0%;
+		left: 0%;
+		right: 0%;
+		bottom: 0%;
+		padding: 50%;
+		z-index: -1;
+	}
+`;
+const ImageContainer = styled.div`
+	position: relative;
+	min-height: 100vh;
+	min-width: calc(100vw-10px);
+	padding: 0 calc(3.5vw + 5px);
+	overflow-x: hidden;
+	overflow-y: hidden;
+`;
---
+++ src/components/Viewers.js
@@ -0,0 +1,15 @@
+ import React from 'react'
+ 
+ function Viewers() {
+     return (
+         <div>
+            <Container>
+
+            </Container>
+         </div>
+     )
+ }
+ 
+ export default Viewers
+ 
+ const Container;
