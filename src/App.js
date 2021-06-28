a248fe79b7f6135284f1a24a227cee7170c75308
ButtonGame <Mandapk255@gmail.com>
2021-06-28 11:51:31 -0700

    Viewers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Viewers from "./components/Viewers";
 
 function App() {
 	return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -56,6 +56,7 @@ const NavMenu = styled.div`
 	flex: 1;
 	margin-left: 25px;
 	align-items: center;
+	overflow-x: hidden;
 	a {
 		display: flex;
 		align-items: center;
