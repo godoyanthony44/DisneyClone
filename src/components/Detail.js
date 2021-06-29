b14c946cc8053ec0a785e3b3a6228f4a67444f29
ButtonGame <Mandapk255@gmail.com>
2021-06-29 09:05:06 -0700

    Interactive Page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,36 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import db from "../firebase";
 
 function Detail() {
+	const { id } = useParams();
+	const [movie, setMovie] = useState();
+	useEffect(() => {
+		db.collection("movies")
+			.doc(id)
+			.get()
+			.then((doc) => {
+				if (doc.exists) {
+					setMovie(doc.data());
+				} else {
+				}
+			});
+	}, [id]);
+
 	return (
 		<Container>
-			<Background>
-				<img
-					id='detailphoto'
-					src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg'
-				/>
-			</Background>
-			<ImageTitle>
-				<img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78'></img>
-			</ImageTitle>
-			<Controls>
-				<PlayButton>
-					<img src='/images/play-icon-black.png'></img>
-					<span>PLAY</span>
-				</PlayButton>
-				<TrailerButton>
-					<img src='/images/play-icon-white.png'></img>
-					<span>TRAILER</span>
-				</TrailerButton>
-				<AddButton>
-					<span>+</span>
-				</AddButton>
-				<GroupWatchButton>
-					<img src='/images/group-icon.png'></img>
-				</GroupWatchButton>
-			</Controls>
-			<SubTitle>2018 - 7m - Family, Fantasy, Kids, Animation</SubTitle>
-			<Description>Description</Description>
+			{movie && (
+				<>
+					<Background>
+						<img id='detailphoto' src={movie.backgroundImg} />
+					</Background>
+					<ImageTitle>
+						<img src={movie.titleImg}></img>
+					</ImageTitle>
+					<Controls>
+						<PlayButton>
+							<img src='/images/play-icon-black.png'></img>
+							<span>PLAY</span>
+						</PlayButton>
+						<TrailerButton>
+							<img src='/images/play-icon-white.png'></img>
+							<span>TRAILER</span>
+						</TrailerButton>
+						<AddButton>
+							<span>+</span>
+						</AddButton>
+						<GroupWatchButton>
+							<img src='/images/group-icon.png'></img>
+						</GroupWatchButton>
+					</Controls>
+					<SubTitle>{movie.subTitle}</SubTitle>
+					<Description>{movie.description}</Description>
+				</>
+			)}
 		</Container>
 	);
 }
